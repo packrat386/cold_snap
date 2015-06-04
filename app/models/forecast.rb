aa3ac35658354d3962d4e7f90973cfb29f12e00f
Aidan Coyle <packrat386@gmail.com>
2015-06-04 09:49:00 -0500

    Added rough layout of data pull

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -10,18 +10,37 @@ class Forecast < ActiveRecord::Base
   private
 
   def pull
+    doc = xml_doc
+    doc.xpath("//parameters//temperature[@type='minimum']//value").each do |val|
+      puts val.content
+    end
+  end
+
+  def xml_doc
+    base_url = "http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdXMLclient.php?"
+    url_s = base_url + nws_now + nws_next_week + URI.encode_www_form(params).to_s
+    puts url_s
+    Nokogiri::XML(open(url_s))
+  end
+  
+  def nws_now
+    "&begin=#{Time.new().utc.strftime('%FT%T')}"
+  end
+
+  def nws_next_week
+    week = Time.new() + (60*60*24*8)
+    "&end=#{week.utc.strftime('%FT%T')}&"
+  end
+
+  def params
     params = {}
-    params[:lat] = 41.83
-    params[:lon] = -87.68
+    # TODO: add user location
+    params[:zipCodeList] = 60604
     params[:product] = "time-series"
-    params[:begin] = Time.new().utc.strftime("%FT%T%:z")
-    week = Time.new() + (60*60*24*7)
-    params[:end] = week.utc.strftime("%FT%T%:z")
-    params[:wx]=:wx
-    base_url = "http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdXMLclient.php"
-    url = URI.join(base_url, URI.encode_www_form(params))
-    puts url.to_s
-    doc = Nokogiri::XML(open(url.to_s))
-    doc.inspect
+    params[:mint] = :mint
+    params[:maxt] = :maxt
+    params[:temp] = :tempfor
+    params[:pop12] = :pop12
+    params
   end
 end
